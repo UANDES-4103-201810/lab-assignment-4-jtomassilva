a7413565df51479dfb0e31378599e71392ac487b
Ignacio Parrague <ibparrague@miuandes.cl>
2018-04-03 13:53:40 -0300

    commit

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,5 @@
 class Place < ApplicationRecord
 	has_many :events
+	validates :name, length: { minimum: 20 }
+	validates :capacity, format: {with: /[0-9]{1,9}\b/ }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-  	has_many :user_tickets	
+	has_many :user_tickets	
 	has_many :tickets, :through => :user_tickets
+	validates :password, length: { in: 8..12 }
+	validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/,
+															 message: "invalid email" }
+	validates :phone, format: { with: /[0-9]{9,12}\b/,
+																		message: "invalid phone number" }
 end
